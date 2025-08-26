5df921a2786fa1e625a376fd5a3993e94a2c9603
Francisco Colmenero <franciscojcolmenero@outlook.com>
2025-08-25 19:33:33 -0500

    Finished enum menu & Classes

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ using std::cin;
 using std::endl;
 using std::string;
 
-enum Category {
-    Food, Transportation, Entertainment, Utilities, Other
+enum class Category {
+    food, transportation, entertainment, utilities, other, exit, invalid
 };
 
 struct CustomDate {
@@ -37,64 +37,181 @@ struct CustomDate {
         if (this->year != rhs.year){
             return (this->year > rhs.year ) ? true : false;
         }
+        else if (this->month != rhs.month) {
+            return (this->month > rhs.month) ? true : false;
+        }
+        else if (this->day != rhs.day) {
+            return (this->day > rhs.day) ? true : false;
+        }
+        return false;
     }
 
     bool operator<(const CustomDate& rhs) const {
-        
+        if (this->year != rhs.year){
+            return (this->year < rhs.year ) ? true : false;
+        }
+        else if (this->month != rhs.month) {
+            return (this->month < rhs.month) ? true : false;
+        }
+        else if (this->day != rhs.day) {
+            return (this->day < rhs.day) ? true : false;
+        }
+        return false;
     }
 };
+
+class MoneyAmount {
+    public: 
+        MoneyAmount(){
+            this->money = 0.0;
+        }
+        MoneyAmount(double amount){
+            this->money = amount;
+        }
+        
+        string getFormattedAmount(){
+            return string("$") + std::to_string(money);
+        }
+
+        double getMoney(){
+            return money;
+        }
+
+        void setMoney(double money){
+            this->money = money;
+        }
+    private:
+        double money;
+
+};
+
 class Transaction{ //Base class
     public:
+        Transaction(double value){
+            amount = MoneyAmount(value);
+        }
+
         double virtual calculateImpact() = 0; //Pure virtual
 
         void virtual displayTransaction() { //Virtual
             cout << "Basic Info" << endl;
         }
 
-    private: 
-        double amount;
+        MoneyAmount getAmount(){
+            return amount;
+        }
+
+    protected: 
+        MoneyAmount amount;
         string description;
         string date;
         Category category;
 
 
-}
+};
 
-int printMenu();
+class IncomeTransaction : public Transaction{
+    public:
+        double calculateImpact() override {
+            return amount.getMoney();
+        }
+
+        void displayTransaction() override{
+            cout << "Earned " << amount.getFormattedAmount() << " from Income" << endl;
+        }
+
+    private:
+
+};
+
+class ExpenseTransaction : public Transaction{
+    public:
+        double calculateImpact() override {
+            return amount.getMoney();
+        }
+
+        void displayTransaction() override{
+            cout << "Spent " << amount.getFormattedAmount() << " from Expenses" << endl;
+        }
+
+    private:
+
+};
+
+string printMenu();
 
 int transactionManagement();
+
+string stringToLower();
+
+Category stringToCategory();
+
+
 
 
 int main(){
     cout << "Welcome to your financial tracker" << endl;
-    int selection = 0;
+    string selection;
+    bool running = true;
+
     do{
         selection = printMenu();
-        switch(selection){
-            case 1:
+        Category category = stringToCategory(selection);
+        switch(category){
+            case Category::food:
                 break;
-            case 2:
+            case Category::transportation:
                 break;
-            case 3:
+            case Category::entertainment:
                 break;
-            case 4:
+            case Category::utilities:
+                break;
+            case Category::other:
+                break;
+            case Category::exit:
+                cout << "Exiting the program." << endl;
+                break;
+            case Category::invalid:
+                cout << "Invalid input, please type of one of the options" << endl;
+                running = false;
                 break;
         }
 
-    } while(selection != 5);
+    } while(running);
 
     return 0;
 }
 
-int printMenu(){
-    int selection = 0;
-    while (selection <= 0 || selection > 5) {
-        cout << "Select an option from out menu: " << endl;
-        cout << "1.) Add Transaction, 2.) View All 2.) View by Category 3.) Analytics 4.) Save/Load 5.) Exit" << endl;
-        cout << "Selection: ";
-        cin >> selection;
-    }
+string printMenu(){
+    string selection;
+    cout << "Select an option from out menu: " << endl;
+    cout << "Transaction, View All Transactions, View by Category, Analytics, Save/Load, Exit" << endl;
+    cout << "Selection: ";
+    cin >> selection;
     return selection;
 }
 
+string stringToLower(string input){
+    for (char& c : input) {
+        c = std::tolower(static_cast<unsigned char>(c));
+    }
+    return input;
+}
+
+Category stringToCategory(string input){
+    static const std::unordered_map<string, Category> table {
+        {"food",  Category::food},
+        {"transportation",  Category::transportation},
+        {"entertainment",  Category::entertainment},
+        {"utilities",  Category::utilities},
+        {"other",  Category::other}, 
+        {"exit",  Category::exit},
+        {"invalid",  Category::invalid},       
+    };
+    input = stringToLower(input);
+    auto it = table.find(input); //Look in table
+    if (it != table.end())
+        return it->second;
+    return Category::invalid;
+}
 
