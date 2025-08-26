5c6283588dd49e7663f48b62b47f131ec02e4ae3
Francisco Colmenero <franciscojcolmenero@outlook.com>
2025-08-25 20:51:03 -0500

    Implemented type T functions & fix bugs

--- main.cpp
+++ main.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <stdexcept>
+#include <unordered_map>
+#include <cctype> 
+
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
 
-enum class Category {
-    food, transportation, entertainment, utilities, other, exit, invalid
+enum class MenuOptions {
+    transaction, view, analytics, save, load, exit, invalid
 };
+
+enum class Category {
+    food, transportation, entertainment, utilities, other
+};
+
+template<typename T>
+T findMax(const std::vector<T>& data);
+
+template<typename T>
+double calculateAverage(const std::vector<T>& data);
+
+string printMenu();
+
+int transactionManagement();
+
+string stringToLower();
+
+MenuOptions stringToMenuOptions();
 
 struct CustomDate {
     int day;
@@ -22,6 +45,7 @@ struct CustomDate {
         this->day = day;
         this->month = month;
         this->year = year;
+        return true;
     }
 
     bool operator==(const CustomDate& rhs) const {
@@ -60,6 +84,24 @@ struct CustomDate {
     }
 };
 
+struct CategoryStats {
+    string name;
+    double totalSpent;
+    int transactionCount;
+    double percentageBudget;
+};
+
+template<typename T>
+class Statistics{
+    private:
+        vector<T> data;
+    public: 
+        void addData(const T& item) { data.push_back(item); }
+        T getMax() const { return findMax(data);}
+        double getAverage() const { return calculateAverage(data); }
+        size_t getCount() const{ return data.size(); }
+};
+
 class MoneyAmount {
     public: 
         MoneyAmount(){
@@ -80,6 +122,24 @@ class MoneyAmount {
         void setMoney(double money){
             this->money = money;
         }
+
+        MoneyAmount operator+(MoneyAmount& rhs){
+            return MoneyAmount(this->money + rhs.getMoney());
+        }
+
+        MoneyAmount operator-(MoneyAmount& rhs){
+            return MoneyAmount(this->money - rhs.getMoney());
+        }
+
+        bool operator==(MoneyAmount& rhs){
+            return (this->money == rhs.getMoney()) ? true : false;
+        }
+
+        friend std::ostream& operator<<(std::ostream& os, const MoneyAmount& money){
+            os << money.getFormattedAmount();
+            return os;
+        }
+        
     private:
         double money;
 
@@ -87,8 +147,11 @@ class MoneyAmount {
 
 class Transaction{ //Base class
     public:
-        Transaction(double value){
-            amount = MoneyAmount(value);
+        Transaction(double value, string description, CustomDate date, Category category){
+            this->amount = MoneyAmount(value);
+            this->description = description;
+            this->date = date;
+            this->category = category;
         }
 
         double virtual calculateImpact() = 0; //Pure virtual
@@ -104,7 +167,7 @@ class Transaction{ //Base class
     protected: 
         MoneyAmount amount;
         string description;
-        string date;
+        CustomDate date;
         Category category;
 
 
@@ -112,6 +175,9 @@ class Transaction{ //Base class
 
 class IncomeTransaction : public Transaction{
     public:
+        IncomeTransaction(double value, string description, CustomDate date, Category category)
+            : Transaction(value, description, date, category) {}
+        
         double calculateImpact() override {
             return amount.getMoney();
         }
@@ -127,7 +193,7 @@ class IncomeTransaction : public Transaction{
 class ExpenseTransaction : public Transaction{
     public:
         double calculateImpact() override {
-            return amount.getMoney();
+            return -amount.getMoney();
         }
 
         void displayTransaction() override{
@@ -138,15 +204,6 @@ class ExpenseTransaction : public Transaction{
 
 };
 
-string printMenu();
-
-int transactionManagement();
-
-string stringToLower();
-
-Category stringToCategory();
-
-
 
 
 int main(){
@@ -156,22 +213,22 @@ int main(){
 
     do{
         selection = printMenu();
-        Category category = stringToCategory(selection);
+        MenuOptions category = stringToMenuOptions(selection);
         switch(category){
-            case Category::food:
+            case MenuOptions::transaction:
                 break;
-            case Category::transportation:
+            case MenuOptions::view:
                 break;
-            case Category::entertainment:
+            case MenuOptions::analytics:
                 break;
-            case Category::utilities:
+            case MenuOptions::save:
                 break;
-            case Category::other:
+            case MenuOptions::load:
                 break;
-            case Category::exit:
+            case MenuOptions::exit:
                 cout << "Exiting the program." << endl;
                 break;
-            case Category::invalid:
+            case MenuOptions::invalid:
                 cout << "Invalid input, please type of one of the options" << endl;
                 running = false;
                 break;
@@ -198,20 +255,42 @@ string stringToLower(string input){
     return input;
 }
 
-Category stringToCategory(string input){
-    static const std::unordered_map<string, Category> table {
-        {"food",  Category::food},
-        {"transportation",  Category::transportation},
-        {"entertainment",  Category::entertainment},
-        {"utilities",  Category::utilities},
-        {"other",  Category::other}, 
-        {"exit",  Category::exit},
-        {"invalid",  Category::invalid},       
+MenuOptions stringToMenuOptions(string input){
+    static const std::unordered_map<string, MenuOptions> table {
+        {"transaction",  MenuOptions::transaction},
+        {"view",  MenuOptions::view},
+        {"analytics",  MenuOptions::analytics},
+        {"save",  MenuOptions::save},
+        {"load",  MenuOptions::load}, 
+        {"exit",  MenuOptions::exit},
+        {"invalid",  MenuOptions::invalid},       
     };
     input = stringToLower(input);
     auto it = table.find(input); //Look in table
     if (it != table.end())
         return it->second;
-    return Category::invalid;
+    return MenuOptions::invalid;
 }
 
+template<typename T>
+T findMax(const std::vector<T>& data){
+    if (data.size() == 0){
+        throw std::runtime_error("Vector is empty, Cannot find max");
+    }
+    T max = data[0];
+    for (auto d : data){
+        if (d > max){
+            max = d;
+        }
+    }
+    return max;
+}
+
+template<typename T>
+double calculateAverage(const std::vector<T>& data){
+    double total = 0;
+    for (int i = 0; i < data.size(); i++){
+        total += data[i];
+    }
+    return (double)total / (double)data.size();
+}
